13498a7864499dc1db93a6d831d8ddcc769b2f09
soufiane oucherrou <soufiane.oucherrou@gmail.com>
2019-09-19 19:12:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,8 +2,8 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	hash = {:railroads => ""}
-p	hash.has_key?(:railroads)
-	
+	hash.has_key?(:railroads)
+	hash
 	
 end
 
