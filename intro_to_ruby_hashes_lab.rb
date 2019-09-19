7c48e1d880fb1cac732510f0d14fdba0364924c3
soufiane oucherrou <soufiane.oucherrou@gmail.com>
2019-09-19 19:09:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,7 +2,8 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	hash = {:key => ""}
-	p hash
+	hash
+	p self
 end
 
 def monopoly_with_second_tier
